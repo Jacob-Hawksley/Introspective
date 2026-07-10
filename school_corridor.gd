bbfe0bc28a549765ec090dbdad484dd61667e004
Jacob-Hawksley <jacobmhawksley@gmail.com>
2026-07-10 11:32:23 +0100

    Added sprite and animation for jerod in the door cutscene

--- school_corridor.gd
+++ school_corridor.gd
@@ -5,8 +5,9 @@ func cutscene(delta):
 	await get_tree().create_timer(2).timeout
 	Main.movementoverride = Vector2(0,0)
 	$Sprite2D.play("open")
-	$Interactions/classdoor/AnimatedSprite2D.global_position = $Interactions/classdoor/CollisionShape2D.global_position
-	$Interactions/classdoor/AnimatedSprite2D.visible = true
+	$Interactions/classdoor/jerod.global_position = $Interactions/classdoor/CollisionShape2D.global_position
+	$Interactions/classdoor/jerod.visible = true
+	$Interactions/classdoor/jerod.play("new_animation")
 func _ready() -> void:
 	$Sprite2D.play("closed")
 func _process(delta: float) -> void:
